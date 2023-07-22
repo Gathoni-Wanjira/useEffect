be0fa308620c4fb8cb254b4cd8e7cb8c7a30b235
Gathoni Wanjira <janetgathoni910@gmail.com>
2023-07-22 11:39:46 +0300

    Data Fetching in react

--- src/App.js
+++ src/App.js
@@ -1,20 +1,28 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
 
     const [ResourceType, setResourceType] = useState('posts')
-  return (
-    <>
-    <div >
-        <button onClick={() => setResourceType('posts')}>Posts</button>
-        <button onClick={() => setResourceType('users')}>Users</button>
-        <button onClick={() => setResourceType('comments')}>Comments</button>
-     
-    </div>
-    <h1>{ResourceType}</h1>
-    </>
-  );
+
+    console.log("Render")
+
+    useEffect(() => {
+        fetch(`https://jsonplaceholder.typicode.com/${ResourceType}`)
+            .then(response => response.json())
+            .then(json => console.log(json))
+    }, [ResourceType])
+    return (
+        <>
+            <div >
+                <button onClick={() => setResourceType('posts')}>Posts</button>
+                <button onClick={() => setResourceType('users')}>Users</button>
+                <button onClick={() => setResourceType('comments')}>Comments</button>
+
+            </div>
+            <h1>{ResourceType}</h1>
+        </>
+    );
 }
 
 export default App;
